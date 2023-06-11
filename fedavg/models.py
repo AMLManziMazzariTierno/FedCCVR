fb52aa34152d2ac674a8660a30963745f3fb5fd2
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-06-11 08:38:46 +0200

    fix: flatten the features

--- fedavg/models.py
+++ fedavg/models.py
@@ -199,7 +199,7 @@ class ReTrainModel(nn.Module):
         super(ReTrainModel, self).__init__()
 
         #Layer 7  classifier layer
-        self.fc = nn.Linear(64,100)
+        self.linear = nn.Linear(64,100)
 
     def forward(self, input):
 
